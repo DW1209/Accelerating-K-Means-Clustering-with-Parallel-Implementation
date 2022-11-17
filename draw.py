317a538124793d445b217e5273d9016b1c24ce57
dw1209 <daniel20001209@gmail.com>
2022-11-18 01:05:21 +0800

    Add default value for the arguments

--- draw.py
+++ draw.py
@@ -8,24 +8,27 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Draw the scatterplots before and after K-Means Clustering.')
-    parser.add_argument('-c', '--clusters', type=int, help='number of clusters', required=True)
-    parser.add_argument('-f', '--filename', type=str, help='<FILENAME> from the inputs directory', required=True)
+    parser.add_argument('-c', '--clusters', type=int, help='classify the data into <CLUSTERS> groups')
+    parser.add_argument('-f', '--filename', type=str, help='<FILENAME> from the inputs directory')
     args, _ = parser.parse_known_args()
+
+    clusters = 3 if args.clusters is None else args.clusters
+    filename = 'data.txt' if args.filename is None else args.filename
 
     # draw settings
     fig, ax = plt.subplots(1, 2, figsize=(16, 8))
     fig.suptitle('Before and After K-Means Clustering')
 
     # before
-    df = pd.read_csv(os.path.join('inputs', args.filename), delim_whitespace=True, header=None)
+    df = pd.read_csv(os.path.join('inputs', filename), delim_whitespace=True, header=None)
     df.columns = ['x', 'y']
     sns.scatterplot(ax=ax[0], x=df['x'], y=df['y'])
     ax[0].set_title('Before Clustering')
 
     # after
-    df = pd.read_csv(os.path.join('outputs', f'{args.filename}.out'), delim_whitespace=True, header=None)
+    df = pd.read_csv(os.path.join('outputs', f'{filename}.out'), delim_whitespace=True, header=None)
     df.columns = ['x', 'y', 'cluster']
-    sns.scatterplot(ax=ax[1], x=df['x'], y=df['y'], hue=df['cluster'], palette=sns.color_palette('hls', n_colors=args.clusters))
+    sns.scatterplot(ax=ax[1], x=df['x'], y=df['y'], hue=df['cluster'], palette=sns.color_palette('hls', n_colors=clusters))
     ax[1].set_title('After Clustering')
 
     plt.tight_layout()
--- main.cpp
+++ main.cpp
@@ -6,38 +6,33 @@
 #include "kmeans.h"
 
 void usage(const char *progname) {
-    fprintf(stderr, "usage: %s [-h] -f FILENAME -c M\n", progname);
+    fprintf(stderr, "usage: %s [-h] [-c CLUSTERS] [-f FILENAME]\n", progname);
     fprintf(stderr, "\n");
     fprintf(stderr, "optional arguments:\n");
     fprintf(stderr, "  -h --help                  show this help message and exit\n");
+    fprintf(stderr, "  -c --clusters <CLUSTERS>   classify the data into <CLUSTERS> groups\n");
     fprintf(stderr, "  -f --filename <FILENAME>   <FILENAME> in the inputs directory\n");
-    fprintf(stderr, "  -c --clusters <M>          set <M> clusters\n");
 }
 
 int main(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"help"      , no_argument      , NULL, 'h'},
-        {"filename"  , required_argument, NULL, 'f'},
-        {"clusters"  , required_argument, NULL, 'c'},
+        {"clusters"  , optional_argument, NULL, 'c'},
+        {"filename"  , optional_argument, NULL, 'f'},
         {NULL        , 0                , NULL,  0 }
     };
 
     int opt;
-    size_t clusters = 0;
-    std::string filename;
-    bool fmark = false, cmark = false;
+    size_t clusters = 3;
+    std::string filename = "data.txt";
 
     while ((opt = getopt_long(argc, argv, "f:c:h", long_options, NULL)) != EOF) {
         switch (opt) {
-            case 'f': filename = std::string(optarg);       fmark = true; break;
-            case 'c': clusters = strtol(optarg, NULL, 10);  cmark = true; break;
+            case 'c': clusters = strtol(optarg, NULL, 10); break;
+            case 'f': filename = std::string(optarg);      break;
             case 'h': usage(argv[0]); exit(1);
             default : usage(argv[0]); exit(1);
         }
-    }
-
-    if (!fmark || !cmark) {
-        usage(argv[0]); exit(1);
     }
 
     DataFrame points;
